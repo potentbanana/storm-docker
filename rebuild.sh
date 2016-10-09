a8dd79acac89b5ce0bd52b251b35f84b56062152
Will Cliffe <will@nerdcorn.com>
2016-10-08 22:02:55 -0700

    Made changes to try and get 1.0.2 storm working.

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t="wurstmeister/storm" storm
-docker build -t="wurstmeister/storm-nimbus" storm-nimbus
-docker build -t="wurstmeister/storm-supervisor" storm-supervisor
-docker build -t="wurstmeister/storm-ui" storm-ui
+docker build -t="storm" storm
+docker build -t="storm-nimbus" storm-nimbus
+docker build -t="storm-supervisor" storm-supervisor
+docker build -t="storm-ui" storm-ui
--- storm/start-supervisor.sh
+++ storm/start-supervisor.sh
@@ -1,6 +1,5 @@
-sed -i -e "s/%zookeeper%/$ZK_PORT_2181_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
-sed -i -e "s/%nimbus%/$NIMBUS_PORT_6627_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
+
+sed -i -e "s/%zookeeper%/zk/g" $STORM_HOME/conf/storm.yaml
+sed -i -e "s/%nimbus%/nimbus/g" $STORM_HOME/conf/storm.yaml
 
 echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
-
-/usr/sbin/sshd && supervisord
