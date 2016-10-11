d9f8d7d299d050cb4258910c37275d2e5e20663c
Will Cliffe <will@nerdcorn.com>
2016-10-10 23:03:02 -0700

    More changes to make this work.

--- storm/start-supervisor.sh
+++ storm/start-supervisor.sh
@@ -1,5 +1,7 @@
-
-sed -i -e "s/%zookeeper%/zk/g" $STORM_HOME/conf/storm.yaml
-sed -i -e "s/%nimbus%/nimbus/g" $STORM_HOME/conf/storm.yaml
+export IP=`hostname -i`
+sed -i -e "s/%zookeeper%/172.18.0.2/g" $STORM_HOME/conf/storm.yaml
+sed -i -e "s/%nimbus%/172.18.0.4/g" $STORM_HOME/conf/storm.yaml
 
 echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
+
+supervisord
