45aa705f674e74e46db448e3f5f5a31b719ddf1f
William Cliffe <will@willcliffe.com>
2018-03-31 22:16:46 -0700

    Upgraded to Apache Storm 1.2.1

--- storm-nimbus/start-supervisor.sh
+++ storm-nimbus/start-supervisor.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
+
 export IP=`hostname -i`
 
-sed -i -e "s/%zookeeper%/172.18.0.2/g" $STORM_HOME/conf/storm.yaml
+sed -i -e "s/%zookeeper%/172.24.0.2/g" $STORM_HOME/conf/storm.yaml
 sed -i -e "s/%nimbus%/$IP/g" $STORM_HOME/conf/storm.yaml
 
 echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
